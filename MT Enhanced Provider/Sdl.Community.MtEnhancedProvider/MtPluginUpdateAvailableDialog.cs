e1f2ced6cc93b7849ad9079a4b509c4e6b615441
slombardo <slombardo@users.noreply.github.com>
2017-06-13 21:49:21 -0400

    - add localization to new changes (#1)

--- MT Enhanced Provider/Sdl.Community.MtEnhancedProvider/MtPluginUpdateAvailableDialog.cs
+++ MT Enhanced Provider/Sdl.Community.MtEnhancedProvider/MtPluginUpdateAvailableDialog.cs
@@ -20,7 +20,24 @@ namespace Sdl.Community.MtEnhancedProvider
             lbl_currentVersion.Text = currentVersion;
             lbl_latestVersion.Text = latestVersion;
             txt_releaseNotes.Text = releaseNotes;
+            LoadResources();
+
         }
+        /// <summary>
+        /// Loads strings to the form from our resources file....defaults have been set in the designer
+        /// </summary>
+        private void LoadResources()
+        {
+            Text = MtPluginUpdateAvailableDialogResources.form_Text;
+            lbl_message.Text = MtPluginUpdateAvailableDialogResources.lbl_message_Text;
+            lbl_current_label.Text = MtPluginUpdateAvailableDialogResources.lbl_current_label_Text;
+            lbl_latestVersion_label.Text = MtPluginUpdateAvailableDialogResources.lbl_latestVersion_label_Text;
+            lbl_releaseNotes.Text = MtPluginUpdateAvailableDialogResources.lbl_releaseNotes_Text;
+            btn_later.Text = MtPluginUpdateAvailableDialogResources.btn_later_Text;
+            btn_skip.Text = MtPluginUpdateAvailableDialogResources.btn_skip_Text;
+            btn_update.Text = MtPluginUpdateAvailableDialogResources.btn_update_Text;
+        }
+
 
         public NewVersionOptions NewVersionOption { get; set; } = NewVersionOptions.RemindMeLater;
 
